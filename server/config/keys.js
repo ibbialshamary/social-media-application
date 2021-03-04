22f20069061c41d0f1d47b87dcb36200bb625396
Ibbi Alshamary <alshamaryi17@gmail.com>
2021-03-04 08:47:57 +0000

    transformed into single-page application for better deployment

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,6 +1,13 @@
-module.exports = {
-    mongoURI: "mongodb+srv://ibbialshamary:ibrahim@cluster0.sesfo.mongodb.net/SocialMediaApplication?retryWrites=true&w=majority",
-    localMongoURI: "mongodb://localhost:27017/SocialMediaApplication",
-    secret: 'password'
-};
-
+if (process.env.NODE_ENV === 'production') {
+    module.exports = {
+        mongoURI: "mongodb+srv://ibbialshamary:ibrahim@cluster0.sesfo.mongodb.net/SocialMediaApplication?retryWrites=true&w=majority",
+        localMongoURI: "mongodb://localhost:27017/SocialMediaApplication",
+        secret: 'password'
+    };
+} else {
+    module.exports = {
+        mongoURI: "mongodb://localhost:27017/SocialMediaApplication",
+        localMongoURI: "mongodb://localhost:27017/SocialMediaApplication",
+        secret: 'password'
+    };
+}
